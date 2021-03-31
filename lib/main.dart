a238530f8bf9705f39f48713d5cd970f0a34aef0
mxsakura <1440717174@qq.com>
2021-03-31 17:46:43 +0800

    flutter-rummygame

---
+++ lib/main.dart
@@ -0,0 +1,304 @@
+import 'package:adjust_sdk/adjust.dart';
+import 'package:adjust_sdk/adjust_config.dart';
+import 'package:adjust_sdk/adjust_event.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_app/pages/details.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AdjustConfig config =
+        new AdjustConfig('k1ocmqx4zke8', AdjustEnvironment.sandbox);
+    Adjust.start(config);
+    print('Adjust初始化成功');
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(255, 127, 0, 1.0),
+          title: Container(
+            alignment: Alignment.center,
+            child: Text('HOME'),
+          ),
+        ),
+        body: Home_page(),
+      ),
+      routes: {
+        //路由注册
+        '/jump': (context) => AppDetails(),
+      },
+    );
+  }
+}
+
+class People {
+  String name;
+  int age;
+
+  People(this.name, this.age);
+}
+
+class Home_page extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RequestSwiper();
+}
+
+class RequestSwiper extends State<Home_page> {
+  String BaseURL = 'https://s.nooapp.com/page';
+  List banners = [];
+  List recommended = [];
+  List hotgames = [];
+
+  @override
+  initState() {
+    // TODO: implement initState
+    super.initState();
+    getBanners();
+  }
+
+  getBanners() async {
+    var response = await http.get("https://s.nooapp.com/page/app.json");
+    if (response.statusCode == 200) {
+      Map map = json.decode(response.body);
+
+      setState(() {
+        banners = map["banner"];
+        recommended = map["games"].take(3).toList();
+        hotgames = map["games"].skip(3).toList();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (banners.length == 0) {
+      return LoadingPage();
+    } else {
+      return ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          Container(
+            height: 200,
+            child: new Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return CachedNetworkImage(
+                  imageUrl:  BaseURL + banners[index]["image"],
+                  placeholder: (context, url) => SpinKitFadingCircle(
+                    color: Color.fromRGBO(255, 127, 0, 1.0),
+                    size: 30.0,
+                  ),
+                  fit: BoxFit.fill,
+                );
+              },
+              pagination: new SwiperPagination(),
+              itemCount: banners.length,
+              viewportFraction: 0.8,
+              scale: 0.9,
+              autoplay: true,
+              onTap: (index) {
+                print(index);
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+            alignment: Alignment.center,
+            child: Text(
+              'Recommended',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            //添加数字索引，同时访问key和value
+            children: recommended.map<Widget>((item) {
+              return InkWell(
+                child: Container(
+                  width: 75,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(18.75),
+                          child: CachedNetworkImage(
+                            imageUrl: BaseURL + item["icon"],
+                            placeholder: (context, url) => SpinKitFadingCircle(
+                              color: Color.fromRGBO(255, 127, 0, 1.0),
+                              size: 30.0,
+                            ),
+                            fit: BoxFit.fill,
+                            height: 75,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          item["name"],
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          item["applied"] + ' applied',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  print('recommended');
+                  AdjustEvent adjustEvent = new AdjustEvent(item["trackEvent"]);
+                  Adjust.trackEvent(adjustEvent);
+                  print(item["trackEvent"] + '事件跟踪成功');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AppDetails(
+                        game: item,
+                      ),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Text(
+              'Hot Games',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //添加数字索引，同时访问key和value
+            children: hotgames.map<Widget>((item) {
+              return Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: InkWell(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(18.75),
+                            child: CachedNetworkImage(
+                              imageUrl: BaseURL + item["icon"],
+                              placeholder: (context, url) =>
+                                  SpinKitFadingCircle(
+                                color: Color.fromRGBO(255, 127, 0, 1.0),
+                                size: 30.0,
+                              ),
+                              fit: BoxFit.fill,
+                              width: 75,
+                              height: 75,
+                            ),
+                          ),
+                        ),
+                        Container(
+                            width: 250,
+                            margin: EdgeInsets.only(left: 20, top: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: Text(
+                                    item["name"],
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    item["applied"] + ' applied',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                )
+                              ],
+                            ))
+                      ],
+                    ),
+                    onTap: () {
+                      print('hot game');
+                      AdjustEvent adjustEvent =
+                          new AdjustEvent(item["trackEvent"]);
+                      Adjust.trackEvent(adjustEvent);
+                      print(item["trackEvent"] + '事件跟踪成功');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AppDetails(
+                            game: item,
+                          ),
+                        ),
+                      );
+                    },
+                  ));
+            }).toList(),
+          )
+        ],
+      );
+    }
+  }
+}
+
+class LoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        new Padding(
+          padding: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 35.0),
+          child: new Center(
+            child: SpinKitFadingCircle(
+              color: Color.fromRGBO(255, 127, 0, 1.0),
+              size: 30.0,
+            ),
+          ),
+        ),
+        new Padding(
+          padding: new EdgeInsets.fromLTRB(0.0, 35.0, 0.0, 0.0),
+          child: new Center(
+            child: new Text('loading'),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/details.dart
@@ -0,0 +1,271 @@
+import 'package:adjust_sdk/adjust.dart';
+import 'package:adjust_sdk/adjust_event.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter_app/widget/previewimg.dart';
+
+class AppDetails extends StatefulWidget {
+  Map game;
+
+  AppDetails({Key key, this.game}) : super(key: key);
+
+  @override
+  _AppDetails createState() => _AppDetails(game: this.game);
+}
+
+class _AppDetails extends State<AppDetails> {
+  Map game;
+  String BaseURL = 'https://s.nooapp.com/page';
+
+  _AppDetails({this.game});
+
+  VideoPlayerController _controller;
+
+  get support => null;
+
+  //重写类方法initState()，初始化界面
+  @override
+  void initState() {
+    super.initState();
+    var videos =
+        game["media"].where((media) => media["type"] == "video").toList();
+    print(videos);
+    //设置视频参数 (..)是级联的意思
+    if (videos.length > 0) {
+      _controller = VideoPlayerController.network(BaseURL + videos[0]["url"])
+        ..initialize().then((_) {
+          // 确保在初始化视频后显示第一帧，直至在按下播放按钮。
+          setState(() {});
+        })
+        ..setLooping(true)
+        ..play();
+    }
+  }
+
+  //打开外部浏览器
+  _openBrower(String url) async {
+    print(url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(255, 127, 0, 1.0),
+        title: Text(""),
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(18.75),
+                  child: CachedNetworkImage(
+                    imageUrl: BaseURL + game["icon"],
+                    placeholder: (context, url) => SpinKitFadingCircle(
+                      color: Color.fromRGBO(255, 127, 0, 1.0),
+                      size: 30.0,
+                    ),
+                    fit: BoxFit.fill,
+                    width: 75,
+                  ),
+                ),
+              ),
+              Container(
+                  width: 250,
+                  margin: EdgeInsets.only(left: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Text(
+                          game["name"],
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Text(
+                          game["applied"] + ' applied',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  )),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+              child: Text(
+                'Download',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255), fontSize: 18),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Color.fromRGBO(255, 127, 0, 1.0),
+                ),
+              ),
+              onPressed: () {
+                print(game["trackEventD"]);
+                AdjustEvent adjustEvent = new AdjustEvent(game["trackEventD"]);
+                Adjust.trackEvent(adjustEvent);
+                print(game["trackEventD"] + '事件跟踪成功');
+                _openBrower(game["srcUrl"]);
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Row(
+                  children: game["media"].asMap().keys.map<Widget>((index) {
+                    if (game["media"][index]["type"] == "image") {
+                      return InkWell(
+                        child: Container(
+                          margin: index < (game["media"].length - 1)
+                              ? EdgeInsets.only(right: 10)
+                              : EdgeInsets.only(right: 0),
+                          child: CachedNetworkImage(
+                            imageUrl: BaseURL + game["media"][index]["url"],
+                            placeholder: (context, url) => SpinKitFadingCircle(
+                              color: Color.fromRGBO(255, 127, 0, 1.0),
+                              size: 30.0,
+
+                            ),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        onTap: () {
+                          var currentImg =
+                              BaseURL + game["media"][index]["url"];
+                          var images = game["media"]
+                              .where((media) => media["type"] == "image")
+                              .toList();
+                          var photoList = images
+                              .map((item) => BaseURL + item["url"])
+                              .toList()
+                              .cast<String>();
+                          var initialPage = photoList.indexOf(currentImg);
+                          print(photoList is List<String>);
+                          print(initialPage);
+                          Navigator.of(context).push(PageRouteBuilder(
+                              pageBuilder: (c, a, s) => PreviewImagesWidget(
+                                    photoList,
+                                    initialPage: initialPage,
+                                  )));
+                        },
+                      );
+                    } else {
+                      return Container(
+                        margin: index < (game["media"].length - 1)
+                            ? EdgeInsets.only(right: 10)
+                            : EdgeInsets.only(right: 0),
+                        child: _controller.value.initialized
+                            ? AspectRatio(
+                                aspectRatio: _controller.value.aspectRatio,
+                                child: VideoPlayer(_controller),
+                              )
+
+                            //如果视频没有加载好或者因网络原因加载不出来则返回Container 组件
+                            //一般用于放置过渡画面
+                            : Container(
+                                child: LoadingPage(),
+                              ),
+                      );
+                    }
+                  }).toList(),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Text(
+              'Details',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              game["details"],
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    var videos =
+        game["media"].where((media) => media["type"] == "video").toList();
+    if (videos.length > 0) {
+      _controller.dispose();
+    }
+  }
+}
+
+class LoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        new Padding(
+          padding: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 35.0),
+          child: new Center(
+            child: SpinKitFadingCircle(
+              color: Color.fromRGBO(255, 127, 0, 1.0),
+              size: 30.0,
+            ),
+          ),
+        ),
+        new Padding(
+          padding: new EdgeInsets.fromLTRB(0.0, 35.0, 0.0, 0.0),
+          child: new Center(
+            child: new Text('loading'),
+          ),
+        ),
+      ],
+    );
+  }
+}
